a7a6c8da17c6b76f95606afff21cb5c961b29969
Pesan <persenzhou@126.com>
2015-10-10 20:47:04 +0800

    Initial Commit

---
+++ 画图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  画图
+//
+//  Created by zhoupushan on 15/10/10.
+//  Copyright © 2015年 zhoupushan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
